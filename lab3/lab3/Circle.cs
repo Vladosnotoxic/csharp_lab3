f04a766dedafc86ef54723868c728a29742c69f8
Vladosnotoxic <94399193+Vladosnotoxic@users.noreply.github.com>
2022-04-21 21:20:35 +0300

    Add files via upload

---
+++ lab3/lab3/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Circle : Figure
+    {
+        public int Raduis { get; set; }
+
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawEllipse(new Pen(Color), Position.X, Position.Y, Raduis, Raduis);
+            gr.DrawString(GetCenter().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+
+        public override double GetArea()
+        {
+            return 3.14 * Raduis * Raduis;
+        }
+
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Raduis / 2), (int)(Position.Y + Raduis / 2));
+        }
+    }
+}
---
+++ lab3/lab3/Deca.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lab3
+{
+    internal class Deca : Figure
+    {
+        public float Side { get; set; }
+
+        public override double GetArea()
+        {
+            return (Side * Side) / 4 * Math.Sqrt(25 + 10 * Math.Sqrt(5));
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X), (int)(Position.Y));
+        }
+        public override void Draw(Graphics gr)
+        {
+            double angle = -Math.PI * 0.5;
+            PointF point1 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 1 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 1 / 10) * Side));
+            PointF point2 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 2 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 2 / 10) * Side));
+            PointF point3 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 3 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 3 / 10) * Side));
+            PointF point4 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 4 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 4 / 10) * Side));
+            PointF point5 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 5 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 5 / 10) * Side));
+            PointF point6 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 6 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 6 / 10) * Side));
+            PointF point7 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 7 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 7 / 10) * Side));
+            PointF point8 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 8 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 8 / 10) * Side));
+            PointF point9 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 9 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 9 / 10) * Side));
+            PointF point10 = new PointF(Position.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * 10 / 10) * Side), Position.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * 10 / 10) * Side));
+
+            PointF[] curvePoints =
+            {
+                 point1,
+                 point2,
+                 point3,
+                 point4,
+                 point5,
+                 point6,
+                 point7,
+                 point8,
+                 point9,
+                 point10
+            };
+
+            gr.DrawPolygon(new Pen(Color), curvePoints);
+
+            gr.DrawString(GetCenter().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+}
